2b94c87f7abb14be97989b346dbc7989e2634c43
Noah Johnson <jnoahjohnson@gmail.com>
2021-10-13 17:22:19 -0600

    A prayer

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 import sveltePreprocess from "svelte-preprocess";
 
-import adapter from "@sveltejs/adapter-vercel";
+import adapter from "@sveltejs/adapter-netlify";
 
 /** @type {import('@sveltejs/kit').Config} */
 export default {
