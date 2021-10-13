202e10b13b09e592bfb5223cb9f2e44db5bc4ad0
Noah Johnson <jnoahjohnson@gmail.com>
2021-10-13 17:28:21 -0600

    FIx

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 import sveltePreprocess from "svelte-preprocess";
 
-import adapter from "@sveltejs/adapter-node";
+import node from "@sveltejs/adapter-node";
 
 /** @type {import('@sveltejs/kit').Config} */
 export default {
@@ -15,16 +15,13 @@ export default {
     // By default, `npm run build` will create a standard Node app.
     // You can create optimized builds for different platforms by
     // specifying a different adapter
-    adapter: adapter(),
+    adapter: node(),
 
     // hydrate the <div id="svelte"> element in src/app.html
     target: "#svelte",
 
     vite: {
       server: {
-        ssr: {
-          noExternal: ["@supabase/supabase-js"],
-        },
         hmr: {
           clientPort: process.env.HMR_HOST ? 443 : 24678,
           host: process.env.HMR_HOST
